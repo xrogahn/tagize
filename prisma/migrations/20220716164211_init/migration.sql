6e6f25645f0da852b5d933247b5bac15d1b295d1
yuarakawa <yu.arakawa@nicosys.jp>
2022-07-17 01:50:09 +0900

    create prisma migration

---
+++ prisma/migrations/20220716164211_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
