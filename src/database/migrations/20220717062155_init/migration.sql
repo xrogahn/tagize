7e30cc5241086fe224ee046ac860db62930970a1
yuarakawa <yu.arakawa@nicosys.jp>
2022-07-17 20:13:36 +0900

    update user table

--- src/database/migrations/20220717062155_init/migration.sql
+++ src/database/migrations/20220717062155_init/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
-CREATE TABLE "Users" (
+CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "isActive" BOOLEAN NOT NULL DEFAULT true,
 
-    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
