3d327ada8bd1fe17ad77be2b819acb60c8554e58
yuarakawa <yu.arakawa@nicosys.jp>
2022-07-17 20:44:01 +0900

    add prefix of endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './module/app.module';
 
+const globalPrefix = '/api';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix(globalPrefix);
+    await app.listen(3000);
 }
 bootstrap();
